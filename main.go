9712ca8b63cd0902a4e67d6a3a814c1872c76dd1
awoelf <98348782+awoelf@users.noreply.github.com>
2024-04-04 00:57:24 -0500

    removed goroutines

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"log"
-	"os/exec"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
-	cmd := exec.Command("/bin/sh", "refresh.sh")
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{
 		Views: engine,
@@ -23,6 +21,6 @@ func main() {
 		}, "layouts/main")
 	})
 	
-	go cmd.Run()
-	go log.Fatal(app.Listen(":3000"))
+	
+	log.Fatal(app.Listen(":3000"))
 }
