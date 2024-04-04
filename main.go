dcf1cfa564dc8192fc866a4071ec2ae8b7d742fe
awoelf <98348782+awoelf@users.noreply.github.com>
2024-04-04 01:22:17 -0500

    successfully deployed with docker image

--- main.go
+++ main.go
@@ -2,25 +2,26 @@ package main
 
 import (
 	"log"
+	"os/exec"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
+	cmd := exec.Command("/bin/sh", "refresh.sh")
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 	app.Static("/", "./public")
 
-
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", fiber.Map{
 			"title": "Add Your Comment!",
 		}, "layouts/main")
 	})
-	
-	
-	log.Fatal(app.Listen(":3000"))
+
+	go cmd.Run()
+	go log.Fatal(app.Listen(":3000"))
 }
